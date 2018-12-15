1f26a7ebd7b83d9eaabdcd5504938f42935e0d74
Juan Pablo <juanpabloguleal@gmail.com>
2018-12-15 00:16:40 -0600

    Add project files.

---
+++ RecProyect/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecProyect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
