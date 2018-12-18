140ae3b73e9eaa5039657aeb66a82fc6b581ed7d
Juan Pablo <juanpabloguleal@gmail.com>
2018-12-17 21:49:55 -0600

    V0.1.1 The user now is able to edit and delete places.

--- RecProyect/Controllers/TplacesController.cs
+++ RecProyect/Controllers/TplacesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RecProyect.Data;
 using RecProyect.Models;
 
@@ -43,6 +44,63 @@ namespace RecProyect.Controllers
                 _db.Add(place);
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
+            }
+            return View(place);
+        }
+
+        //POST: Place/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePlace(int id)
+        {
+            var place = await _db.Places.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Places.Remove(place);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        //Delete : Place/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var place = await _db.Places.SingleOrDefaultAsync( m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+            return View(place);
+        }
+
+        //POST: Place/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Place place)
+        {
+            if (id != place.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(place);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(place);
+        }
+        //Edit : Place/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var place = await _db.Places.SingleOrDefaultAsync(m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
             }
             return View(place);
         }
